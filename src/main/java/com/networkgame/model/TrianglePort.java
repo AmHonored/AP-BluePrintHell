8392f2da5c6bbbd2914cfbfe19e9106aef1a24d5
AmHonored <lordbehradsl@gmail.com>
2025-05-06 15:40:00 +0000

    Implement port system with specialized port types

---
+++ src/main/java/com/networkgame/model/TrianglePort.java
@@ -0,0 +1,39 @@
+package com.networkgame.model;
+
+import javafx.geometry.Point2D;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
+
+public class TrianglePort extends Port {
+    private static final double PORT_SIZE = 10;
+    
+    public TrianglePort(Point2D position, boolean isInput, NetworkSystem system) {
+        super(position, isInput, Packet.PacketType.TRIANGLE, system);
+        
+        // Create the triangle shape
+        Polygon triangle = new Polygon();
+        triangle.getPoints().addAll(0.0, 0.0, 0.0, 0.0, 0.0, 0.0); // Placeholder points
+        triangle.setFill(isInput ? Color.LIGHTGREEN : Color.GREEN);
+        triangle.setStroke(Color.BLACK);
+        this.shape = triangle;
+        
+        // Update position if it was provided
+        if (position != null) {
+            updateTrianglePoints(triangle, position);
+        }
+    }
+    
+    private void updateTrianglePoints(Polygon triangle, Point2D pos) {
+        triangle.getPoints().setAll(
+            pos.getX(), pos.getY() - PORT_SIZE,     // Top
+            pos.getX() - PORT_SIZE * 0.866, pos.getY() + PORT_SIZE * 0.5,  // Bottom left
+            pos.getX() + PORT_SIZE * 0.866, pos.getY() + PORT_SIZE * 0.5   // Bottom right
+        );
+    }
+    
+    @Override
+    protected void updateShapePosition() {
+        Polygon triangle = (Polygon) shape;
+        updateTrianglePoints(triangle, position);
+    }
+} 
